c633566aa0b4859f7fd4bb21fffd190ebb07a97a
Dguykc1 <kezechristian@gmail.com>
2022-06-20 11:34:18 +0100

    set the home page

--- src/main/java/codeblaze/cardealersite/controllers/HomeController.java
+++ src/main/java/codeblaze/cardealersite/controllers/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-    @GetMapping("/carviews/index")
+    @GetMapping("/index")
     public String getHomePage(Model model){
         return "/carviews/index";
     }
